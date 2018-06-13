28435b96c83752eb22a202327f2b751ac5caf2a8
majidkazemi89 <36406173+majidkazemi89@users.noreply.github.com>
2018-06-13 15:46:55 +0200

    IsectionPoint documented

--- sources/include/IsectionPoint.hh
+++ sources/include/IsectionPoint.hh
@@ -3,7 +3,7 @@
 #include "TObject.h"
 #include "TString.h"
 #include "TVector3.h"
-
+///Class which gets intersection point coordinates and global bin number to be used in class CCMLEM.  
 class IsectionPoint : public TObject{
   
 public:
@@ -13,22 +13,28 @@ public:
   IsectionPoint(Int_t bin, Double_t x, Double_t y, Double_t z);
   ~IsectionPoint();
   
+  ///Sets coordinates of intesection point and global bin number
   void SetBinPoint(Int_t bin, Double_t x, Double_t y, Double_t z);
+  ///Sets coordinates of intesection point.
   void SetPointCoordinates(Double_t x, Double_t y, Double_t z);
+  ///Sets global bin number.
   void SetBin(Int_t b);
+  
   void Print(void);
   
-  TVector3* GetPointCoordinates(void); //{ return fPoint; };
-  Int_t GetBin(void) const; //{ return fGlobalBin; };
+  ///Returns coordinates of intesection point.
+  TVector3* GetPointCoordinates(void);
+  ///Returns global bin number.
+  Int_t GetBin(void) const; 
   
 private:
   
  
-  TVector3 *fPoint;
-  Int_t    fGlobalBin;
+  TVector3 *fPoint;				///< Coordinates of the interaction point
+  Int_t    fGlobalBin;				///< Global bin number
   
-  Int_t  Compare(const TObject* run2) const;
-  Bool_t IsSortable() const {return kTRUE;};
+  Int_t  Compare(const TObject* run2) const;	///< Compares sortable objects (global bin numbers)
+  Bool_t IsSortable() const {return kTRUE;};	///< Returns kTRUE for all sortable objects
   
   
   
