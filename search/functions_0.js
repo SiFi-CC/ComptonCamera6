4b68fd11ed56d8fce13ebfc732b04a904792828c
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2018-08-21 14:59:35 +0200

    new version of documentation

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['accesssetup',['AccessSetup',['../classInputReaderGeant.html#ac7246a35483e30447c76c7454293830c',1,'InputReaderGeant']]],
   ['accesstree',['AccessTree',['../classInputReader.html#a629654ede9710c1f6b05db9da6fc619e',1,'InputReader::AccessTree()'],['../classInputReaderGeant.html#ae5ffeea241af265f6a4ce83b9e362149',1,'InputReaderGeant::AccessTree()'],['../classInputReaderSimple.html#a26912b5b576a9951ee630ba3c5f92c77',1,'InputReaderSimple::AccessTree()']]]
 ];
