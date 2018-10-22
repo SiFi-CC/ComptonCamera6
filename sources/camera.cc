49ea50146c81ae1ea1ced5662939d886f8cecd88
majidkazemi89 <kazemi.ma89@gmail.com>
2018-10-22 11:16:27 +0200

    .

--- sources/camera.cc
+++ sources/camera.cc
@@ -5,11 +5,11 @@ using namespace std;
 int main(void){
 
   Int_t nev = 100000;
-  Int_t gen = 4;
+  Int_t gen = 1;
   
   CCSimulation *sim = new CCSimulation(Form("CCSimulation_gen%i",gen),kFALSE);
   sim->SetGenVersion(gen);
-  sim->BuildSetup(200,80,80,250,300,300);
+  sim->BuildSetup(230,80,80,460,300,300);
   sim->Loop(nev);
   delete sim;
    
