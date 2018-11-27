a35ee87ee3a2555de4e3f09d49ed103f9278f6dc
Wojciech Kozyra <wkozyra95@gmail.com>
2018-11-28 00:00:03 +0100

    add source implementations(line and point)

---
+++ common/lib/Sources/PointSource.cc
@@ -0,0 +1,34 @@
+#include "PointSource.hh"
+#include <TRandom.h>
+
+// -----------------------
+// PointSource
+// -----------------------
+
+Track* PointSource::GenerateEvent() {
+  Double_t angleY = gRandom->Uniform(-fAngleY, fAngleY);
+  Double_t angleZ = gRandom->Uniform(-fAngleZ, fAngleZ);
+  TVector3 versor = TVector3(1, tan(angleY), tan(angleZ)).Unit();
+  return new Track(fPosition, versor, fEnergy, "point_source_track");
+}
+
+// -----------------------
+// MultiPointSource
+// -----------------------
+
+Track* MultiPointSource::GenerateEvent() {
+  Track* track = fIterator->GenerateEvent();
+  track->SetPoint(track->GetPoint() + fPosition);
+
+  if (fIterator == fSources.end()) {
+    fIterator = fSources.begin(); // on last element go to start
+  } else {
+    ++fIterator;
+  }
+  return track;
+}
+
+void MultiPointSource::AddSourceElement(const PointSource& source) {
+  fSources.push_back(source);
+  fIterator = fSources.begin();
+}
