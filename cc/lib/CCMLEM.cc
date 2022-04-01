b5bbb02b4c791038370dc2951a742c2443760c92
Monika Łukowicz <monika.lukowicz@student.uj.edu.pl>
2022-04-01 09:50:50 +0200

    Debugging

--- cc/lib/CCMLEM.cc
+++ cc/lib/CCMLEM.cc
@@ -1534,27 +1534,27 @@ Bool_t CCMLEM::DrawHisto(void)
 
     int lastiter = fIter;
 
-    TCanvas* can_allIterations = new TCanvas("MLEM2D_allIterations","MLEM2D_allIterations",1000,1000);
-    TCanvas* can_allIterations_z  = new TCanvas("MLEM1DZ_allIterations","MLEM1DZ_allIterations",1000,1000);
-    TCanvas* can_allIterations_y  = new TCanvas("MLEM1DY_allIterations","MLEM1DY_allIterations",1000,1000);
+    // TCanvas* can_allIterations = new TCanvas("MLEM2D_allIterations","MLEM2D_allIterations",1000,1000);
+    // TCanvas* can_allIterations_z  = new TCanvas("MLEM1DZ_allIterations","MLEM1DZ_allIterations",1000,1000);
+    // TCanvas* can_allIterations_y  = new TCanvas("MLEM1DY_allIterations","MLEM1DY_allIterations",1000,1000);
 
-    can_allIterations->DivideSquare(20);
-    can_allIterations_z->DivideSquare(20);
-    can_allIterations_y->DivideSquare(20);
+    // can_allIterations->DivideSquare(20);
+    // can_allIterations_z->DivideSquare(20);
+    // can_allIterations_y->DivideSquare(20);
 
-    for(int iter = 1; iter < fIter +1; iter++) {
-        can_allIterations->cd(iter);
-        cout << "iteration number " << iter << ", current pad: " << gPad->GetName() << endl;
-        fImage[iter]->Draw("colz");
-        can_allIterations_z->cd(iter);
-        fImage[iter]->ProjectionX()->Draw();
-        can_allIterations_y->cd(iter);
-        fImage[iter]->ProjectionY()->Draw();
-    }
+    // for(int iter = 1; iter < fIter +1; iter++) {
+    //     can_allIterations->cd(iter);
+    //     cout << "iteration number " << iter << ", current pad: " << gPad->GetName() << endl;
+    //     fImage[iter]->Draw("colz");
+    //     can_allIterations_z->cd(iter);
+    //     fImage[iter]->ProjectionX()->Draw();
+    //     can_allIterations_y->cd(iter);
+    //     fImage[iter]->ProjectionY()->Draw();
+    // }
 
-    can_allIterations->Write();
-    can_allIterations_z->Write();
-    can_allIterations_y->Write();
+    // can_allIterations->Write();
+    // can_allIterations_z->Write();
+    // can_allIterations_y->Write();
 
     TH1D* hProZ[250];
     TH1D* hProY[250];
