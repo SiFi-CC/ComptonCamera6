7116b1087411cc2a64b44bde6fb0cda6469b83d1
jonas.kasper <kasper@physik.rwth-aachen.de>
2022-07-13 16:29:46 +0200

    Comments to the file structure for the InputReaderNN

--- common/lib/InputReaderNN.hh
+++ common/lib/InputReaderNN.hh
@@ -37,40 +37,40 @@ private:
   int fCorrectOnly; // 0 all results from the NN, 1 loading eventtype 1 and 2, 2 loading eventtype 2, 3 only eventtype 0, 4 only eventtype 1 
   list<int> fSelectedEvents;
   
-  float fX1;
-  float fY1;
-  float fZ1;
-  float fX2;
-  float fY2;
-  float fZ2;
-  float fX3;
-  float fY3;
-  float fZ3;
-  float fVX;
-  float fVY;
-  float fVZ;
-  float fVUncX;
-  float fVUncY;
-  float fVUncZ;
-  float fPX;
-  float fPY;
-  float fPZ;
-  float fPUncX;
-  float fPUncY;
-  float fPUncZ;
-  float fE0Calc;
-  float fE0CalcUnc;
-  float fArc;
-  float fArcUnc;
-  float fE1;
-  float fE1Unc;
-  float fE2;
-  float fE2Unc;
-  float fE3;
-  float fE3Unc;
-  int fClassID;
-  int fEventType;
-  int fEnergyBinID;
+  float fX1;///< x dimension of electron interaction 
+  float fY1;///< y dimension of electron interaction
+  float fZ1;///< z dimension of electron interaction
+  float fX2;///< x dimension of photon interaction
+  float fY2;///< y dimension of photon interaction
+  float fZ2;///< z dimension of photon interaction
+  float fX3;///< x dimension of third interaction in the detector, artifact from the file form from Luebeck here always =0 
+  float fY3;///< y dimension of third interaction in the detector, artifact from the file form from Luebeck here always =0
+  float fZ3;///< z dimension of third interaction in the detector, artifact from the file form from Luebeck here always =0
+  float fVX;///< x dimension of electron interaction, the same as x1 
+  float fVY;///< y dimension of electron interaction, the same as y1
+  float fVZ;///< z dimension of electron interaction, the same as z1
+  float fVUncX;///< uncertainty of the x dimension of electron interaction, in the case of the current NN always =0 
+  float fVUncY;///< uncertainty of the y dimension of electron interaction, in the case of the current NN always =0
+  float fVUncZ;///< uncertainty of the z dimension of electron interaction, in the case of the current NN always =0
+  float fPX;///< x dimension of photon interaction, the same as x2
+  float fPY;///< y dimension of photon interaction, the same as y2
+  float fPZ;///< z dimension of photon interaction, the same as z2
+  float fPUncX;///< uncertainty of the x dimension of photon interaction, in the case of the current NN always =0 
+  float fPUncY;///< uncertainty of the y dimension of photon interaction, in the case of the current NN always =0
+  float fPUncZ;///< uncertainty of the z dimension of photon interaction, in the case of the current NN always =0
+  float fE0Calc; ///< primary energy calculated from electron energy+photon energy (leaf is artifact from 3 interaction model from Luebeck)
+  float fE0CalcUnc; ///< uncertainty to E0Calc, here always =0
+  float fArc;///< scattering angle calculated from electron energy and photon energy
+  float fArcUnc;///< uncertainty to Arc, here always =0
+  float fE1;///< energy of the electron
+  float fE1Unc;///< uncertainty to E1, here always =0
+  float fE2;///< energy of the photon 
+  float fE2Unc;///< uncertainty to E2, here always =0
+  float fE3;///< energy of the third interaction in the detector (3 interaction model to recover full energy), artefact from Luebeck 
+  float fE3Unc;///< uncertainty to E3, here always =0
+  int fClassID;///< Artefact from Luebeck, not meaningfull here 
+  int fEventType;///< Quality of the event: =0 nether energy not position correct, =1 position correct energy not, =2 energy and position correct 
+  int fEnergyBinID;///< Artefact from Luebeck, not meaningfull here 
   
   TVector3* fPositionScat;  ///< Position of interaction in scatterer
   TVector3* fPositionAbs;   ///< Position of interaction in absorber
